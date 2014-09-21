a785e5d47c09ab1a04c0d5de20bbb26e3997e3f0
B1g0n3 <B1g0n3@B1g0n3-PC>
2014-09-21 23:12:44 +0200

    Buil 2

--- MyWeather/src/com/myweather/MainActivity.java
+++ MyWeather/src/com/myweather/MainActivity.java
@@ -23,67 +23,32 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 	public double latitude;
 	public double longitude;
 	TextView mCurrentTemp;
-    private TextView mLocationDate;
-    private TextView mLocationLong;
-    private TextView mLocationLat;
+    private TextView mStatus;
 
-    private TextView mPreviousLocationDate;
-    private TextView mPreviousLocationLong;
-    private TextView mPreviousLocationLat;
-
-	
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	    ReconSDKManager mDataManager   = ReconSDKManager.Initialize(this);
-		mCurrentTemp = (TextView) findViewById(R.id.current_temp);
-		mLocationDate = (TextView) findViewById(R.id.id_loctime);
-		mLocationLong = (TextView) findViewById(R.id.id_loclong);
-		mLocationLat  = (TextView) findViewById(R.id.id_loclat);
-		mPreviousLocationDate = (TextView) findViewById(R.id.id_locprevtime);
-		mPreviousLocationLong = (TextView) findViewById(R.id.id_locprevlong);
-		mPreviousLocationLat  = (TextView) findViewById(R.id.id_locprevlat);
-
-		LocationManager locationManager;
-		String context = Context.LOCATION_SERVICE;
-		locationManager = (LocationManager)getSystemService(context);
-		 
-		Criteria mycriteria = new Criteria();
-		mycriteria.setAccuracy(Criteria.ACCURACY_FINE);
-		mycriteria.setAltitudeRequired(false);
-		mycriteria.setBearingRequired(false);
-		mycriteria.setCostAllowed(true);
-		mycriteria.setPowerRequirement(Criteria.POWER_LOW);
-		String provider = locationManager.getBestProvider(mycriteria, true);
-		 
-		Location location = locationManager.getLastKnownLocation(provider);
-		updateWithNewLocation(location);
-//		locationManager.requestLocationUpdates(provider, 1000, 0, locationListener); 
-		
-//		mDataManager.receiveData(this, ReconEvent.TYPE_LOCATION);
-		
+		mDataManager.receiveData(this, ReconEvent.TYPE_LOCATION);
 	}
+    
+    protected void onStart() {
+    	super.onStart();
+    }
 
 	public void onReceiveCompleted(int status, ReconDataResult result)
 	{
-		
 	    // check status first
 	    if (status != ReconSDKManager.STATUS_OK)
 	    {
-	Toast toast = Toast.makeText(this,"Communication Failure with Transcend Service",Toast.LENGTH_LONG);
+	    	Toast toast = Toast.makeText(this,"Communication Failure with Transcend Service",Toast.LENGTH_LONG);
 	        toast.show();
 	        System.out.println("Communication Failure with Transcend Service");
 	        return;
 	    }
-	    
-	 // Grab on-board temperature sensor reading and convert to Fahrenheit
-//	    ReconTemperature temp = (ReconTemperature)result.arrItems.get(0);
-//	    System.out.println("temperature="+temp.GetTemperature());
-	         
-	    // now simply dump data into text fields
+	    System.out.println("ReconSDKManager.STATUS_OK");
 	    ReconLocation rloc = (ReconLocation)result.arrItems.get(0);
-
 	    Location loc = rloc.GetLocation();
 	    Location prevloc = rloc.GetPreviousLocation();
 
@@ -91,47 +56,24 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 	    if (loc != ReconLocation.INVALID_LOCATION)
 	    {
 	        Date locdate = new Date (loc.getTime() );
-	        mLocationDate.setText(locdate.toString() );
-
-	        String strText = String.format(" %.2f", loc.getLatitude() );
-	        mLocationLat.setText(strText);
-	        System.out.println("latitude="+strText);
-
-	        strText = String.format(" %.2f", loc.getLongitude() );
-	        mLocationLong.setText(strText);
-	        System.out.println("longitude="+strText);
-
+	        System.out.println("locationdate="+locdate.toString() );
+	        latitude=loc.getLatitude();
+	        longitude=loc.getLongitude();
+	        System.out.println("latitude="+latitude);
+	        System.out.println("longitude="+longitude);
+			mStatus = (TextView) findViewById(R.id.status);
+			mStatus.setText("Lat:"+latitude+" / long:"+longitude);
+			
 	    }
 	    else
 	    {
-	        mLocationDate.setText("No GPS Fix");
 	        System.out.println("No GPS Fix");
-
 	    }
-
-
 	}
 
-	private void updateWithNewLocation(Location location) {
-		System.out.println("On receive3");
-	    String latLong;
-//	    TextView myLocation;
-//	    myLocation = (TextView) findViewById(R.id.myLocation);
-	 
-	    if(location!=null) {
-	        latitude = location.getLatitude();
-	        longitude = location.getLongitude();
-	        latLong = "Latitude:      " + latitude + "\nLongitude:  " + longitude;     
-	    } else {
-	        latLong = "Unable to obtain a GPS Location Fix";
-	    }
-	    System.out.println("result="+latLong);
-	    }
-	
 	@Override
 	public void onFullUpdateCompleted(int arg0, ArrayList<ReconDataResult> arg1) {
 		// TODO Auto-generated method stub
-		System.out.println("On receive2");
-		
+		System.out.println("On receive4");
 	}
 }
