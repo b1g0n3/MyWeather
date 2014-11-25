4e6cb928f52e192fbebb1b2af9936ec8066ab045
B1g0n3 <B1g0n3@B1g0n3-PC>
2014-11-25 23:30:35 +0100

    Build 10

--- MyWeather/src/com/myweather/MainActivity.java
+++ MyWeather/src/com/myweather/MainActivity.java
@@ -1,31 +1,26 @@
 package com.myweather;
 
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.github.dvdme.ForecastIOLib.FIOCurrently;
+import com.github.dvdme.ForecastIOLib.ForecastIO;
 import com.reconinstruments.ReconSDK.*;
 import com.reconinstruments.webapi.IReconHttpCallback;
 import com.reconinstruments.webapi.ReconHttpRequest;
 import com.reconinstruments.webapi.ReconHttpResponse;
 import com.reconinstruments.webapi.ReconOSHttpClient;
-import com.reconinstruments.webapi.IReconHttpCallback.ERROR_TYPE;
 
 import android.app.Activity;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +33,7 @@ public class MainActivity extends Activity implements IReconDataReceiver {
     public boolean first;
 	private TextView textView;
 	private ReconOSHttpClient client;
+	String key = "28faca837266a521f823ab10d1a45050";
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +43,14 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 		StrictMode.setThreadPolicy(policy); 
 		textView = (TextView) findViewById(R.id.text_view);
 		textView.setMovementMethod(new ScrollingMovementMethod());
+		client = new ReconOSHttpClient(this, clientCallback);
+    	first=true;
+		Toast.makeText(this, "Reading GPS", Toast.LENGTH_SHORT).show();
+		ReconSDKManager mDataManager   = ReconSDKManager.Initialize(this);
+		mDataManager.receiveData(this, ReconEvent.TYPE_LOCATION);
 
-	    CustomAsyncTask myCustomTask = new CustomAsyncTask(this);
-		// Execution de la tâche, on lui passe le paramètre à traiter
-		// Execute the task, we give it the parameter to work with
-		myCustomTask.execute("Params to execute in background");
-
-		
-   }
+	}
+    
     
     @Override
 	protected void onDestroy() {
@@ -62,95 +58,6 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 		client.close();
 	}
 
-    public class CustomAsyncTask extends AsyncTask<String, Integer, Integer>{
-    	private Activity  mContext;
-    	private ProgressBar pbAsyncBackgroundTraitement;
-     
-    	public CustomAsyncTask(Activity  mContext) {
-    		super();
-    		this.mContext = mContext;
-    	}
-     
-    	/* fonction qui est appelé en première */
-    	/* function which is called first */
-    	@Override
-    	protected void onPreExecute() {
-    		this.pbAsyncBackgroundTraitement = (ProgressBar) mContext.findViewById(R.id.pbAsyncBackgroundTraitement);
-    		this.pbAsyncBackgroundTraitement.setVisibility(View.VISIBLE);
-    		super.onPreExecute();
-    	}
-     
-    	/* fonction qui fait le traitement de fond */
-    	/* function which do work in background */
-    	@Override
-    	protected Integer doInBackground(String... params) {
-    		// Ici on fait un traitement inutile, à vous de placer ce que vous avez à faire 
-    		// Here we do useless work, you have to put here what you have to do
-
-    		while (true) {
-//    		for (int i = 0; i< params[0].length(); i++){
-//    			try {
-	    		final ReconSDKManager mDataManager   = ReconSDKManager.Initialize(MainActivity.this);
-	    		mDataManager.receiveData(MainActivity.this, ReconEvent.TYPE_LOCATION);
-	    		textView.setText("Lat:"+latitude+" / long:"+longitude);
-	    		if (latitude!=0) { return 0; }
-	    		
-//  				Thread.sleep(100);
-//    			} catch (InterruptedException e) {
-//  				// TODO Auto-generated catch block
-//    				e.printStackTrace();
-//    			}
-//    			int progress = (i * 100 / params[0].length());
-//    			Log.i("test", " progress : " + progress);
-    			// Cette fonction appelle la fonction onProgressUpdate()
-    			// This function call onProgressUpdate()
-//    			publishProgress(progress);
-    		}
-    	}
-     
-    	/* fonction qui met à jour la progress bar */
-    	/* function which set progress update */
-    	@Override
-    	protected void onProgressUpdate(Integer... values) {
-    		// On met à jour la progress bar
-    		// We update the progress bar
-    		this.pbAsyncBackgroundTraitement.setProgress(values[0]);
-    		super.onProgressUpdate(values);
-    	}
-    	/* fonction qui est appelé après que le traitement effectué en tache de fond est terminé */
-    	/* function which is called after work on background function is over */
-    	@Override
-    	protected void onPostExecute(Integer result) {
-    		// Le traitement est terminé, vous pouvez faire une action comme ouvrir une nouvelle activity ou cacher des composant de l'UI
-    		// The work is done, you can do an action like create an activity or set invisible some UI components
-    		this.pbAsyncBackgroundTraitement.setVisibility(View.GONE);
-    		Toast.makeText(this.mContext, "Work is over... Do what you want now...", Toast.LENGTH_LONG).show();    		
-    		client = new ReconOSHttpClient(MainActivity.this, clientCallback);
-    		latitude=50.61741134643555;
-    		longitude=3.1304383277893066;
-    		textView.setText("Lat:"+latitude+" / long:"+longitude);
-    		String key = "28faca837266a521f823ab10d1a45050";
-    		System.out.println("Lat:"+latitude+" / long:"+longitude);
-    		System.out.println("Fetching data...");
-    		try {
-    			URL url = new URL("https://api.forecast.io/forecast/28faca837266a521f823ab10d1a45050/50.61741134643555,3.1304383277893066");
-    		//	URL url = new URL("http://www.reconinstruments.com");
-    			Map<String, List<String>> headers = new HashMap<String, List<String>>();			
-    			byte[] body = "".getBytes();
-    			sendRequest(new ReconHttpRequest("GET", url, null, null));
-
-    		} catch (MalformedURLException e) {
-    			System.out.println("MalformedURLException...");
-    			Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-    		}
-
-    		
-    		super.onPostExecute(result);
-    	}
-     
-    }
-    
-    
 	public void onReceiveCompleted(int status, ReconDataResult result)
 	{
 		if (first) {
@@ -168,6 +75,20 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 		    {
 		        latitude=loc.getLatitude();
 		        longitude=loc.getLongitude();
+				
+				System.out.println("Lat:"+latitude+" / long:"+longitude);
+				System.out.println("Fetching data...");
+				try {
+					URL url = new URL("https://api.forecast.io/forecast/28faca837266a521f823ab10d1a45050/"+latitude+","+longitude);
+					//50.61741134643555,3.1304383277893066");
+					Map<String, List<String>> headers = new HashMap<String, List<String>>();			
+					byte[] body = "".getBytes();
+					sendRequest(new ReconHttpRequest("GET", url, null, null));
+
+				} catch (MalformedURLException e) {
+					System.out.println("MalformedURLException...");
+					Toast.makeText(this, "erreur http:"+e.getMessage(), Toast.LENGTH_LONG).show();
+				}
 		        first=false;
 		    }
 		    else
@@ -190,8 +111,8 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 	
 	public void sendRequest(ReconHttpRequest request) {
 		if (-1 == client.sendRequest(request)) {
-			Toast.makeText(MainActivity.this, "HUD not connected", Toast.LENGTH_SHORT).show();
-			System.out.println("HUD not connected");
+			Toast.makeText(MainActivity.this, "No Internet", Toast.LENGTH_SHORT).show();
+			System.out.println("HUD not connected - No Internet");
 		} else {
 			Toast.makeText(MainActivity.this, "Request Sent", Toast.LENGTH_SHORT).show();
 			System.out.println("Request Sent");
@@ -203,10 +124,19 @@ public class MainActivity extends Activity implements IReconDataReceiver {
 		public void onReceive(int requestId, ReconHttpResponse response) {
 			System.out.println("Response ready...");
 			Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-			
+			String answer=String(response.getBody());
 			System.out.println(new String(response.getBody()));
 			System.out.println(response.getUrl());
-	//		textView.setText(new String(response.getBody()));
+			ForecastIO fio = new ForecastIO(key);
+			fio.getForecast(answer);
+			FIOCurrently currently = new FIOCurrently(fio);
+			textView.setText("temperature="+currently.get().temperature());
+//			textView.setText(new String(response.getBody()));
+		}
+
+		private String String(byte[] body) {
+			// TODO Auto-generated method stub
+			return null;
 		}
 
 		@Override
